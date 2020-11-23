8df521268d7fa9f841435ff736be93c6335c2971
Cecilia <earwen1986@gmail.com>
2020-11-23 19:40:33 +0000

    added hover effects

--- carousel.js
+++ carousel.js
@@ -23,6 +23,6 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "flex";
   dots[slideIndex-1].className += " active";
 } 
