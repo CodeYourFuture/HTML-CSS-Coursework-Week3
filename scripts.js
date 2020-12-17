41d399ab82312c9aeedbd3b5d9a9141c5fee0877
Kara <kara.mh191@gmail.com>
2020-12-17 18:59:13 +0000

    nav bar & styling for intro
    burger nav bar and styling for intro font and background

---
+++ scripts.js
@@ -0,0 +1,10 @@
+<script>
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+</script>
