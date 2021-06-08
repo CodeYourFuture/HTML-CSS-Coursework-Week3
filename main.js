ce0deac94fd175d9d96c3d5a85819af6ed49a4d0
humailhasankhan <h.hasan.khan88@hotmail.com>
2021-06-08 04:28:57 +0100

    Completed header for mobile devices with html css and a little JavaScript

---
+++ main.js
@@ -0,0 +1,7 @@
+const menu_btn = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mobile-nav");
+
+menu_btn.addEventListener("click", function () {
+  menu_btn.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
+});
