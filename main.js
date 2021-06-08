b2dbe3a323e09a18443736870276cd5e100a8f31
musayuksel <79163385+musayuksel@users.noreply.github.com>
2021-06-08 14:35:17 +0100

    Add margin for hamburger menu

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ let hamburger = document.querySelector('.hamburger');
 // console.log(hamburger);
 let navBar = document.querySelector('header nav');
 // console.log(navBar);
+let header = document.querySelector('header');
 hamburger.addEventListener('click' ,openMenu);
 
  function openMenu() {
    navBar.classList.toggle('open');
+   header.classList.toggle('open');
  }
