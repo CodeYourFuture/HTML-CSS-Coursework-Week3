06f46b5b89000c88d9de0d2978ec6dd484ebf4f3
Anza-Azam <anzaazam.nw4@gmail.com>
2021-06-11 15:21:30 -0700

    experimenting

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-/** @format */
+window.onload = function() {
+  /** @format */
 
-window.onload = function () {
-	const menu_btn = document.querySelector(".hamburger");
-	const mobile_menu = document.querySelector(".mobile-nav");
 
-	menu_btn.addEventListener("click", function () {
-		menu_btn.classList.toggle("is-active");
-		mobile_menu.classList.toggle("is-active");
-	});
+  const menu_btn = document.querySelector(".hamburger");
+  const mobile_menu = document.querySelector(".mobile-nav");
+
+  menu_btn.addEventListener("click", function() {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+  });
+
 };
