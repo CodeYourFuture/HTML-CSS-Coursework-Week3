922c8c27c9917a0c0a4e1a15f04aa6691428495f
Ibrahim Mattar <ibmmtr@gmail.com>
2021-06-13 08:56:04 +0300

    Prepare new editing

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    /** @format */
+  
+  
+    const menu_btn = document.querySelector(".hamburger");
+    const mobile_menu = document.querySelector(".mobile-nav");
+  
+    menu_btn.addEventListener("click", function() {
+      menu_btn.classList.toggle("is-active");
+      mobile_menu.classList.toggle("is-active");
+    });
+  
+  };
