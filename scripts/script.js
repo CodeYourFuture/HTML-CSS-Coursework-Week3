c8a15bc4b5421a863d20295710b43d7583f92693
Berkeli Halmyradov <berkelih@gmail.com>
2021-11-28 18:54:54 +0000

    Added images and applied grid. Added menu toggle for mobile

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+window.onload=function(){
+    document.getElementById("hamburger").addEventListener("click", ()=>{
+        document.getElementById("navigation").classList.toggle("open")
+    })
+}
