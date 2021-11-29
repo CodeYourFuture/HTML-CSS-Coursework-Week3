cc05a1392cee2cc1a24d8ae53b74c2565584d994
Berkeli Halmyradov <berkelih@gmail.com>
2021-11-29 21:58:52 +0000

    Website is fully responsive for 3 viewports.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,8 @@
 window.onload=function(){
     document.getElementById("hamburger").addEventListener("click", ()=>{
         document.getElementById("navigation").classList.toggle("open")
+        document.getElementById("header").classList.toggle("sticky")
+        document.getElementById("hamb-icon").classList.toggle("hidden")
+        document.getElementById("close-icon").classList.toggle("hidden")
     })
 }
