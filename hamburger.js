d573b5dd6399805e0e63a77d665cb8b83aabf6dd
Daniel <pigos487@gmail.com>
2021-12-02 01:35:16 +0100

    end project

---
+++ hamburger.js
@@ -0,0 +1,8 @@
+function hamburgerMenu() {
+    var x = document.getElementById("hamburger");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
