f03db0b562115224c003791503a3b2a88a6392e5
Chelle <dearmichelley@gmail.com>
2022-07-22 12:31:13 +0100

    grid gridified for mobile

---
+++ script.js
@@ -0,0 +1,9 @@
+const navButton = document.querySelector('.expand-nav');
+const navList = document.querySelector('.nav-list');
+console.log(navButton);
+console.log(navList);
+
+navButton.addEventListener('click', function () {
+    console.log('click works');
+
+});
