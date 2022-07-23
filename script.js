709c661a831e6e8bec5daace28c4904e469e4647
Chelle <dearmichelley@gmail.com>
2022-07-23 11:21:05 +0100

    responsive nav fixed

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const navButton = document.querySelector('.expand-nav');
-const navList = document.querySelector('.nav-list');
+const navList = document.querySelector('.responsive-nav');
 
 navButton.addEventListener('click', function () {
 
-    // if (navList.style.display === 'block') {
-    //     navList.style.display = 'none';
-    // } else {
-    //     navList.style.display = 'block';
-    //     navList.style.backgroundColor = '#fff0f5';
-    // }
+    if (navList.style.display === 'block') {
+        navList.style.display = 'none';
+    } else {
+        navList.style.display = 'block';
+    }
 
 });
 
