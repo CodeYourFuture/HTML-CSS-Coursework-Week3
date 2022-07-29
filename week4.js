ac95c64e716e2b9e1bc545d39335e8889be4d5f6
MathoNM <80788322+MathoNM@users.noreply.github.com>
2022-07-29 17:31:03 +0200

    Starting with html

---
+++ week4.js
@@ -0,0 +1,31 @@
+//let years = [1964, 2008, 1999, 2005, 1978, 1985, 1919]
+//years.forEach(function(num)){ 
+//let x = num - 7
+//console.log(x)
+
+let array = [
+    100,
+    "iSMael",
+    55,
+    45,
+    "sANyiA",
+    66,
+    "JaMEs",
+    "eLAmIn",
+    23,
+    "IsMael",
+    67,
+    19,
+    "ElaMIN",
+  ];
+
+  function removeNumbers(element) {
+      if(typeof element === "string") {
+          return element;
+      }
+  }
+
+  function upperCase(name) {
+      return name.toUpperCase() + "!" ;
+
+  }
