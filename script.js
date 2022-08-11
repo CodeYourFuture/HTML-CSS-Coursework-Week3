2eac20502d8ad55f28e3fe67c2cdca891a974bc8
Adibab <adiba.du.bd@gmail.com>
2022-08-12 00:00:42 +0100

    applying media query for min 540px & more .

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ navToggleBtn.addEventListener('click', showNavLinks);
 function showNavLinks (){
     navLinks.classList.toggle('nav-open');
 }
+
+// get the smurf working for footer-btn
+// select the footer btn
+const fotterToggleBTN = document.querySelector('.footer-button');
+const iconLinkName = document.getElementById('iconList');
+// add event listener 
+fotterToggleBTN.addEventListener('click', showFooterlinks);
+// run the function 
+
+function showFooterlinks(){
+    // select the link of footer which is icon
+    
+    iconLinkName.classList.toggle('footer-open');
+}
