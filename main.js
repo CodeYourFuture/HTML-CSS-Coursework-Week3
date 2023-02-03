9cb27a52e1c9c7cf6d133cc3ce770ed81eee455f
Seyyednavid Hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-02-03 01:03:22 +0000

    Responsive project in 3 different sizes

--- main.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("my-topnav");
-  if (x.className === "topnav") {
-    x.className += "responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
